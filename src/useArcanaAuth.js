29b973050a2827562c2ec89dde611c362a6f534e
Surya056 <tejasurya056@gmail.com>
2022-11-15 09:13:42 +0530

    initial commit

---
+++ src/useArcanaAuth.js
@@ -0,0 +1,68 @@
+import {AppMode, AuthProvider} from "@arcana/auth"
+import {useState} from "react"
+
+
+//Config
+const appId = "304B77dD6dF4a929385EAf7AD642015132b8A786";
+
+let auth;
+
+function useArcanaAuth() {
+  const [initialized, setInitialized] = useState(false);
+
+  const initializeAuth = async () => {
+    if (!auth) {
+      auth = new AuthProvider(appId);
+      await auth.init({ appMode: AppMode.NoUI, position: "right" });
+      setInitialized(true);
+    }
+  }
+
+  //Check isLoggedIn
+  const isLoggedIn = async () => {
+    if(initialized) {
+      return await auth.isLoggedIn();
+    }
+  }
+
+  //Social Login
+  
+  const login = async (socialType) => {
+    if(initialized) {
+      await auth.loginWithSocial(socialType);
+    }
+  }
+
+  //Email Link/ Passwordless login
+  const loginWithLink = async (email) => {
+    if(initialized) {
+      await auth.loginWithLink(email);
+    }
+  }
+
+  //Getting user Accounts
+  const getAccounts = async () => {
+    if(initialized) {
+      return await auth.provider.request({ method: "eth_accounts" });
+    }
+  }
+  
+  //Logout
+  const logout = async () => {
+    if(initialized) {
+      return await auth.logout();
+    }
+  }
+
+  return {
+    initializeAuth,
+    isLoggedIn,
+    login,
+    loginWithLink,
+    getAccounts,
+    logout,
+    initialized,
+  };
+}
+
+export default useArcanaAuth;
